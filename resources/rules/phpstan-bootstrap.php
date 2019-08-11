218ad328e81d33edc2afbffef81c84f117b81784
Pablo Reyes <pablo@srtk.com>
2019-08-11 13:52:17 -0300

    auto commit

--- resources/rules/phpstan-bootstrap.php
+++ resources/rules/phpstan-bootstrap.php
@@ -8,4 +8,10 @@
 
 declare(strict_types=1);
 
+$helper_meta_file = __DIR__.'/../../resources/_ide_helper_meta.php';
+//if(!file_exists($helper_meta_file)) {
+//    exec('composer ide-helper');
+//}
+require $helper_meta_file;
+
 require __DIR__.'/../../vendor/nunomaduro/larastan/bootstrap.php';
